274e2a22ad2a8708ede0124d53e20cbe4bbd1dca
JoseEduardoRomero <lalin03032000@gmail.com>
2019-07-11 14:11:09 -0500

    loggin nuevo

--- Emai/public_html/admin/loggin.php
+++ Emai/public_html/admin/loggin.php
@@ -1,16 +1,15 @@
-<?php 
+<?php
 include_once '../backend/modelo/BD.php';
 include_once '../backend/modelo/MAdmin.php';
 include_once '../backend/controlador/CAdmin.php';
 $admin = new CAdmin();
 session_start();
-if( isset($_POST["usuario"]) && isset($_POST["password"])){
-$admin->validarUsuario($_POST["usuario"], $_POST["password"]);    
+if (isset($_POST["usuario"]) && isset($_POST["password"])) {
+    $admin->validarUsuario($_POST["usuario"], $_POST["password"]);
 }
-if (isset($_SESSION["autentificado"])){
-     header("Location: panelAdmin.php");
+if (isset($_SESSION["autentificado"])) {
+    header("Location: panelAdmin.php");
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -24,47 +23,34 @@ and open the template in the editor.
         <title>Aministrador</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <link href="../estilos/estilos.css" rel="stylesheet" type="text/css"/>
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=swap" rel="stylesheet">
         <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="../estilos/style.css" rel="stylesheet" type="text/css"/>
+        <link href="../estilos/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+
     </head>
     <body>
 
-        <div class="formulario-admin">
-        <div class="col-lg-12 mt-5">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 text-center mt-5 card">
-                        <div id="login-logo">
-                            <img alt="" class="img-fluid" src="images/emai 2.png">
-                        </div>
-                        <h1 class="display-4">Administrador</h1>
-                        <form method="post" action="loggin.php">
-                             <div class="input-group mt-3" id="usuario">
-                                <div class="input-group-prepend">
-                                    <div class="input-group-text">
-                                        <i class="fa fa-user mx-auto"></i>
-                                    </div>
-                                </div>
-                                <input type="text" name="usuario" class="form-control" placeholder="Usuario" required="">
-                            </div>
-                            <div class="input-group mt-3">
-                                <div class="input-group-prepend">
-                                    <div class="input-group-text">
-                                        <i class="fa fa-key mx-auto"></i>
-                                    </div>
-                                </div>
-                                <input type="password" name="password" class="form-control" placeholder="Contraseña" required="">
-                            </div>
-                           
-                            <button type="submit" class="btn btn-success col-12 mt-3 mb-5">Entrar</button>
-                        </form>
-                    </div>
+
+
+        <div class="login-box">
+            <h1>Administrador</h1>
+            <form method="post" action="loggin.php">
+                <div class="textbox">
+                    <i class="fas fa-user"></i>
+                    <input type="text" name="usuario">
+                </div>
+                <br>
+                <div class="textbox">
+                    <i class="fas fa-lock"></i>
+                    <input type="password" name="password"> 
                 </div>
 
-            </div>
+                <input type="submit" class="btn" value="Entrar">
+            </form>
         </div>
-        </div>
+
+
 
     </body>
 </html>
