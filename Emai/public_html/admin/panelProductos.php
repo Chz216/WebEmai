bf03573d7bcc28dac792cd8c9c22dc565255f164
JoseEduardoRomero <lalin03032000@gmail.com>
2019-07-11 16:29:43 -0500

    panel-Productos

---
+++ Emai/public_html/admin/panelProductos.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>Productos</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="../estilos/estilos.css" rel="stylesheet" type="text/css"/>
+        <link href="../estilos/font-awesome.css" rel="stylesheet" type="text/css"/>
+        <link href="../estilos/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body class="panel-admin">
+                        <div class="container">
+            <div class="row profile">
+                <div class="col-md-3">
+                    <div class="profile-sidebar">
+
+                        <div class="profile-userpic text-center">
+                            <img src="https://picsum.photos/290/200?random=5" class="img-responsive" alt="">
+                        </div>
+
+                        <div class="profile-usertitle">
+                            <div class="profile-usertitle-name">
+                                Usuario
+                            </div>
+                            <div class="profile-usertitle-job">
+                                Administrador
+                            </div>
+                        </div>
+
+                        <div class="profile-userbuttons">
+
+                            <button type="button" class="btn btn-danger btn-sm">Salir</button>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-md-9">
+                    <div class="profile-content">
+                        <div class="col-lg-12">
+                            <div class="container">
+                                <div class="row">
+
+                                    <div class="col-md-6 mt-3">
+                                        <div class="card profile-card-3">
+                                            <div class="background-block">
+                                                <img src="https://picsum.photos/650/940?random=13"/>
+                                            </div>
+                                            <div class="profile-thumb-block">
+                                                <img src="https://picsum.photos/650/540?random=2" alt="profile-image" class="profile"/>
+                                            </div>
+                                            <div class="card-content">
+                                                <h2>Producto 1</h2>
+                                                <div class="icon-block">
+                                                        <a href="#"><i class="fa fa-pencil-square-o"></i></a>
+                                                        <a href="#"><i class="fa fa-trash" ></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6 mt-3">
+                                        <div class="card profile-card-3">
+                                            <div class="background-block">
+                                                <img src="https://picsum.photos/650/940?random=6"/>
+                                            </div>
+                                            <div class="profile-thumb-block">
+                                                <img src="https://picsum.photos/650/540?random=5" alt="profile-image" class="profile"/>
+                                            </div>
+                                            <div class="card-content">
+                                                <h2>Producto 2</h2>
+                                                <div class="icon-block">
+                                                     
+                                                        <a href="#"><i class="fa fa-pencil-square-o"></i></a>
+                                                        <a href="#"><i class="fa fa-trash" ></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                  <div class="col-md-6 mt-3">
+                                        <div class="card profile-card-3">
+                                            <div class="background-block">
+                                                <img src="https://picsum.photos/650/940?random=20"/>
+                                            </div>
+                                            <div class="profile-thumb-block">
+                                                <img src="https://picsum.photos/650/540?random=19" alt="profile-image" class="profile"/>
+                                            </div>
+                                            <div class="card-content">
+                                                <h2>Producto 3</h2>
+                                                <div class="icon-block">
+                                                     
+                                                        <a href="#"><i class="fa fa-pencil-square-o"></i></a>
+                                                        <a href="#"><i class="fa fa-trash" ></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                     <div class="col-md-6 mt-3">
+                                        <div class="card profile-card-3">
+                                            <div class="background-block">
+                                                <img src="https://picsum.photos/650/940?random=16"/>
+                                            </div>
+                                            <div class="profile-thumb-block">
+                                                <img src="https://picsum.photos/650/540?random=10" alt="profile-image" class="profile"/>
+                                            </div>
+                                            <div class="card-content">
+                                                <h2>Producto 4</h2>
+                                                <div class="icon-block">
+                                                     
+                                                        <a href="#"><i class="fa fa-pencil-square-o"></i></a>
+                                                        <a href="#"><i class="fa fa-trash" ></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                     <div class="col-md-6 mt-3">
+                                        <div class="card profile-card-3">
+                                            <div class="background-block">
+                                                <img src="https://picsum.photos/650/940?random=15"/>
+                                            </div>
+                                            <div class="profile-thumb-block">
+                                                <img src="https://picsum.photos/650/540?random=8" alt="profile-image" class="profile"/>
+                                            </div>
+                                            <div class="card-content">
+                                                <h2>Producto 5</h2>
+                                                <div class="icon-block">
+                                                     
+                                                        <a href="#"><i class="fa fa-pencil-square-o"></i></a>
+                                                        <a href="#"><i class="fa fa-trash" ></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                     <div class="col-md-6 mt-3 mb-4">
+                                        <div class="card profile-card-3">
+                                            <div class="background-block">
+                                                <img src="https://picsum.photos/650/940?random=14"/>
+                                            </div>
+                                            <div class="profile-thumb-block">
+                                                <img src="https://picsum.photos/650/540?random=7" alt="profile-image" class="profile"/>
+                                            </div>
+                                            <div class="card-content">
+                                                <h2>Producto 6</h2>
+                                                
+                                                <div class="icon-block">
+                                                     
+                                                        <a href="#"><i class="fa fa-pencil-square-o"></i></a>
+                                                        <a href="#"><i class="fa fa-trash" ></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <center>
+        <strong>Tienda de Música <a href="http://emai.com" target="_blank">Emai</a></strong>
+    </center>
+
+        </body>
+</html>
+
