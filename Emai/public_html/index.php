313a06cd8c49f3cc75bd86c56414201a1a5cec57
JoseEduardoRomero <lalin03032000@gmail.com>
2019-07-30 20:30:35 -0500

    nuevo carrusel

--- Emai/public_html/index.php
+++ Emai/public_html/index.php
@@ -49,23 +49,41 @@ $cIndex = new CIndex();
             </div>
             
         </nav>
-    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+        
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+        </ol>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="images/comprimida 2.jpg" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
                 <img src="images/comprimida.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>First slide label</h5>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </div>
             </div>
             <div class="carousel-item">
                 <img src="images/comprimida3.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>First slide label</h5>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="images/comprimida 2.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>First slide label</h5>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </div>
             </div>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
             <span class="fa fa-chevron-circle-left" aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
         </a>
-        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
             <span class="fa fa-chevron-circle-right" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
         </a>
