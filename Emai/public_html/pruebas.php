175ad90695496eb638c2a7ab490220b2c61638b1
JoseEduardoRomero <lalin03032000@gmail.com>
2019-07-31 20:08:59 -0500

    mejoras en pantalla de Inicio y borrado de carpeta js (no se utilizaba)

--- Emai/public_html/pruebas.php
+++ Emai/public_html/pruebas.php
@@ -29,7 +29,7 @@ and open the template in the editor.
                             <a class="nav-link" href="index.html">Inicio</span></a>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="mega-menu" data-toggle="dropdown">
+                            <a class="nav-link dropdown nav-item" href="#" id="mega-menu" data-toggle="dropdown">
                                 Productos
                             </a>
                             <div class="dropdown-menu" aria-labelledby="mega-menu">
@@ -96,7 +96,7 @@ and open the template in the editor.
                             <a class="nav-link" href="contacto.html">Contactanos</a>
                         </li>
                     </ul>
-                    <form class="navbar-form" role="search">
+                    <form class="navbar-form navbar-expanded-lg" role="search">
                         <div class="input-group">
                             <input type="text" class="form-control" placeholder="Buscar Productos">
 
@@ -174,13 +174,68 @@ and open the template in the editor.
 
                         </div>
                     </div>
+                    <div class="col-lg-12">
+                        <div class="row">
+
+                            <div class="col-lg-6">
+                                <div id="navigation-pills">
+                                    <div class="row">
+                                        <div class="col-lg-12 col-md-12">
+                                            <div class="row">
+                                                <div class="col-lg-12">
+                                                    <ul class="nav nav-pills nav-pills-icons" role="tablist">
+
+                                                        <li class="nav-item">
+                                                            <a class="nav-link active show" href="#dashboard-1" role="tab" data-toggle="tab" aria-selected="true">
+                                                                <i class="fa fa-check-square-o" aria-hidden="true"></i> Misión
+                                                            </a>
+                                                        </li>
+                                                        <li class="nav-item">
+                                                            <a class="nav-link" href="#schedule-1" role="tab" data-toggle="tab" aria-selected="false">
+                                                                <i class="fa fa-check-circle" aria-hidden="true"></i>Visión
+                                                            </a>
+                                                        </li>
+                                                        <li class="nav-item">
+                                                            <a class="nav-link" href="#tasks-1" role="tab" data-toggle="tab" aria-selected="false">
+                                                                <i class="fa fa-check-circle-o" aria-hidden="true"></i>Objetivo
+                                                            </a>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                                <div class="col-lg-12">
+                                                    <div class="tab-content tab-space">
+                                                        <div class="tab-pane active show" id="dashboard-1">
+
+                                                            <p class="text-justify">Nuestra misión es proporcionar los mejores instrumentos musicales con la más alta calidad y precios accesibles, para satisfacer a nuestros clientes. Queremos que estén satisfechos con nuestros productos y servicios. Brindaremos un mejor sonido y diseños innovadores para que tanto niños, jóvenes y adultos tengan acceso a productos que estén a la vanguardia.</p>
+
+                                                        </div>
+                                                        <div class="tab-pane" id="schedule-1">
+                                                            <p>Nuestra visión es ser la empresa número uno en venta de instrumentos musicales, así como también lograr un mejor sonido y diseños vanguardistas para lograr una mejor imagen en nuestros instrumentos.</p>
+                                                        </div>
+                                                        <div class="tab-pane" id="tasks-1">
+                                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel distinctio dolores impedit cupiditate debitis iure possimus labore voluptate. Ex, quia, possimus explicabo nihil aut eligendi harum maiores repellat tenetur nulla quam commodi nam nemo eos a porro labore illo fuga laudantium dolore? Itaque, aut quas quaerat inventore eos quo sunt.</p> 
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <img src="images/imagen 1.jpg" class="img-fluid shadow">
+                            </div>
+                        </div>
+                    </div>
 
                     <div class="col-lg-12 text-center">
                         <div class="row">
                             <div class="col-lg-12">
                                 <p class="h1">Productos Destacados</p>
                             </div>
-                            <div class="col-lg-3">
+                            <div class="col-lg-4 col-md-4">
                                 <div class="card">
                                     <img class="card-img-top" src="https://images.unsplash.com/photo-1517303650219-83c8b1788c4c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd4c162d27ea317ff8c67255e955e3c8&auto=format&fit=crop&w=2691&q=80" alt="Card image cap">
                                     <div class="card-body">
@@ -189,7 +244,7 @@ and open the template in the editor.
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-lg-3">
+                            <div class="col-lg-4 col-md-4">
                                 <div class="card">
                                     <img class="card-img-top" src="https://images.unsplash.com/photo-1517303650219-83c8b1788c4c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd4c162d27ea317ff8c67255e955e3c8&auto=format&fit=crop&w=2691&q=80" alt="Card image cap">
                                     <div class="card-body">
@@ -198,16 +253,7 @@ and open the template in the editor.
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-lg-3">
-                                <div class="card">
-                                    <img class="card-img-top" src="https://images.unsplash.com/photo-1517303650219-83c8b1788c4c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd4c162d27ea317ff8c67255e955e3c8&auto=format&fit=crop&w=2691&q=80" alt="Card image cap">
-                                    <div class="card-body">
-                                        <p class="card-text text-justify">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <button class="btn btn-primary">Ver más</button>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-3">
+                            <div class="col-lg-4 col-md-4">
                                 <div class="card">
                                     <img class="card-img-top" src="https://images.unsplash.com/photo-1517303650219-83c8b1788c4c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd4c162d27ea317ff8c67255e955e3c8&auto=format&fit=crop&w=2691&q=80" alt="Card image cap">
                                     <div class="card-body">
@@ -216,73 +262,54 @@ and open the template in the editor.
                                     </div>
                                 </div>
                             </div>
+                         
                         </div>
                     </div>
-                    <div class="vision-mision">
-                        <div class="vision mt-3 mb-4 mt-5"> 
-                            <div class="col-lg-12">
-                                <div class="container">
-                                    <div class="row">
-                                        <div class="col-lg-6 col-md-12 text-center">
-                                            <img src="images/imagen bateria comprimida.jpg" class="img-fluid" alt="Responsive image">
+                    
+                </div>
+            </div>
 
-                                        </div>
-                                        <div class="col-lg-6  col-md-12">
 
-                                            <p class="h2">Misión</p>
-                                            <p class="text-justify">Nuestra misión es proporcionar los mejores instrumentos musicales con la más alta calidad y precios accesibles, para satisfacer a nuestros clientes. Queremos que estén satisfechos con nuestros productos y servicios.
-                                                Brindaremos un mejor sonido y diseños innovadores para que tanto niños, jóvenes y adultos tengan acceso a productos que estén a la vanguardia.
-                                            </p>
-                                            <BR> 
-                                            <p class="h2">Visión</p>
-                                            <p class="text-justify">
-                                                Nuestra visión es ser la empresa número uno en venta de instrumentos musicales, así como también lograr un mejor sonido y diseños vanguardistas para lograr una mejor imagen en nuestros instrumentos.</p>
-                                        </div>
 
-                                    </div>
+
+
+            <footer class="pie text-white">
+                <div class="col-lg-12 p-3 mb-2text-white">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-4 col-md-12">
+                                <div class="col-lg-12 text-center">
+                                    <h6 class="lead">TIENDA MUSICAL</h6>    
                                 </div>
+                                <div class="col-lg-12 col-md-12 text-center">
+                                    <img src="images/emai.png" width="120" height="70" alt="">    
+                                </div>
+                                <div class="col-lg-12 text-center">
+                                    <h6 class="lead">CONÓCENOS</h6>
+                                </div>
+
+                            </div>
+                            <div class="col-lg-4 text-center">
+                                <h6 class="lead">ACERCA DE</h6>
+                                <p>Somos una empresa con mas de 5 años de experiencia trabajando en la ciudad de Tehuacan </p>
+                            </div>
+                            <div class="col-lg-4 text-center">
+                                <H6 class="lead">Contáctanos</H6>
+
+
+
+                                <p><a href="https://www.facebook.com/tiendaemai"><span class="fa fa-facebook-square mr-2" style='color:white;'></span></a>Facebook</p>
+
+                                <span class="fa fa-phone mr-2" ></span><a href="tel:+340 38-4-34-67" style='color:white;'>38-4-34-67</a>
+                                <p><span class="fa fa-map-marker mr-2"></span>Centro Tehuacan</p>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-             <footer class="pie text-white">
-            <div class="col-lg-12 p-3 mb-2text-white">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-4 col-md-12">
-                            <div class="col-lg-12 text-center">
-                                <h6 class="lead">TIENDA MUSICAL</h6>    
-                            </div>
-                            <div class="col-lg-12 col-md-12 text-center">
-                                <img src="images/emai.png" width="120" height="70" alt="">    
-                            </div>
-                            <div class="col-lg-12 text-center">
-                                <h6 class="lead">CONÓCENOS</h6>
-                            </div>
-
-                        </div>
-                        <div class="col-lg-4 text-center">
-                            <h6 class="lead">ACERCA DE</h6>
-                            <p>Somos una empresa con mas de 5 años de experiencia trabajando en la ciudad de Tehuacan </p>
-                        </div>
-                        <div class="col-lg-4 text-center">
-                            <H6 class="lead">Contáctanos</H6>
-
-
-
-                            <p><a href="https://www.facebook.com/tiendaemai"><span class="fa fa-facebook-square mr-2" style='color:white;'></span></a>Facebook</p>
-
-                            <span class="fa fa-phone mr-2" ></span><a href="tel:+340 38-4-34-67" style='color:white;'>38-4-34-67</a>
-                            <p><span class="fa fa-map-marker mr-2"></span>Centro Tehuacan</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </footer>
+            </footer>
         </div>
 
-       
+
         <script src="jss/material-kit.min.js" type="text/javascript"></script>
         <script src="jss/core/jquery.min.js" type="text/javascript"></script>
         <script src="jss/core/popper.min.js" type="text/javascript"></script>
